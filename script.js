08cf173ce3c0f061941bc28d04301cfbb98c08b0
Jennifer Browning <browningjen@yahoo.com>
2018-10-22 09:12:44 -0400

    last attempt

---
+++ script.js
@@ -0,0 +1,126 @@
+const columnOne = document.getElementById("column1")
+const columnTwo = document.getElementById("column2")
+const columnThree = document.getElementById("column3")
+const columnFour = document.getElementById("column4")
+const columnFive = document.getElementById("column5")
+const columnSix = document.getElementById("column6")
+const columnSeven = document.getElementById("column7")
+
+
+let gameBoard = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+];
+
+
+const columns = [columnOne, columnTwo, columnThree, columnFour, columnFive, columnSix, columnSeven];
+let clickNum = 0;
+
+
+columns.forEach(function playingBoard(column) {
+    column.addEventListener("click", discDropper)
+})
+
+function discDropper(event) {
+    const column = event.currentTarget;
+
+    if (column.childElementCount === 6) {
+        return;
+    }
+    rowIndex = column.childElementCount;
+    columnIndex = column.dataset.columnNum;
+
+    if (clickNum % 2) {
+
+        let redDisc = document.createElement("div");
+        redDisc.classList.add("redPiece");
+        column.appendChild(redDisc);
+        console.log(gameBoard[columnIndex][rowIndex] = "red");
+
+    } else {
+        let blackDisc = document.createElement("div");
+        blackDisc.classList.add("blackPiece");
+        column.appendChild(blackDisc);
+        console.log(gameBoard[columnIndex][rowIndex] = "black");
+    }
+    clickNum++
+    winChecker();
+}
+
+function winChecker() {
+    if (
+        matchFourInARow(horizontal)
+        || matchFourInARow(vertical)
+        || matchFourInARow(diagonalUpRight)
+        || matchDiagonalUpLeft(diagonalUpLeft)
+    ) {
+        console.log("Winner")
+    }
+}
+
+function matchFourInARow(searchFunction) {
+    for (let columnIndex = 0; columnIndex < gameBoard.length - 3; columnIndex++) {
+        for (let rowIndex = 0; rowIndex < gameBoard[columnIndex].length; rowIndex++) {
+            let cell = gameBoard[columnIndex][rowIndex];
+            if (cell !== 0) {
+                const matched = searchFunction(cell, columnIndex, rowIndex);
+                if (matched) return true;
+            }
+        }
+    }
+    return false;
+}
+
+function matchDiagonalUpLeft(searchFunction) {
+    for (let columnIndex = 3; columnIndex < gameBoard.length; columnIndex++) {
+        for (let rowIndex = 0; rowIndex < gameBoard[columnIndex].length - 3; rowIndex++) {
+            let cell = gameBoard[columnIndex][rowIndex];
+            if (cell !== 0) {
+                const matched = searchFunction(cell, columnIndex, rowIndex);
+                if (matched) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function horizontal(cell, columnIndex, rowIndex) {
+    if (cell === gameBoard[columnIndex + 1][rowIndex] && cell === gameBoard[columnIndex + 2][rowIndex] && cell === gameBoard[columnIndex + 3][rowIndex]) {
+        console.log("winning")
+        return true;
+    }
+
+    return false;
+
+}
+
+function vertical(cell, columnIndex, rowIndex) {
+    if (cell === gameBoard[columnIndex][rowIndex + 1] && cell === gameBoard[columnIndex][rowIndex + 2] && cell === gameBoard[columnIndex][rowIndex + 3]) {
+        return true;
+    }
+
+    return false;
+
+}
+
+function diagonalUpRight(cell, columnIndex, rowIndex) {
+    if (cell === gameBoard[columnIndex + 1][rowIndex + 1] && cell === gameBoard[columnIndex + 2][rowIndex + 2] && cell === gameBoard[columnIndex + 3][rowIndex + 3]) {
+        return true;
+    }
+
+    return false;
+
+}
+
+function diagonalUpLeft(cell, columnIndex, rowIndex) {
+    if (cell === gameBoard[columnIndex - 1][rowIndex + 1] && cell === gameBoard[columnIndex - 2][rowIndex + 2] && cell === gameBoard[columnIndex - 3][rowIndex + 3]) {
+        return true;
+    }
+    return false;
+}
